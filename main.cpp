bc222ff7595cbf3ab99e53dc50667c077fa2522d
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2018-12-04 18:15:56 -0700

    Remove unused headers, add comments

--- main.cpp
+++ main.cpp
@@ -1,25 +1,31 @@
 /***********************************************************************************************
-** Program name: Lab 10
+** Program name: Final Project
 ** Author: Jordan Hamilton
-** Date: 12/1/2018
-** Description: This program displays a menu asking the user to add an integer to the end of a
-** queue in a new node, display the integer data member of the node at the front of the queue,
-** remove the node at the front of the queue, display the values of all nodes in the queue, or
-** quit the program. A method in the Queue object performs the appropriate action on a
-** circular linked list based on the user's choice.
+** Date: 12/4/2018
+** Description: This program seeds a random number generator, then creates new smart pointers
+** to a Game object and a Menu object. The menu prompts the user to begin a game or exit. Once
+** the game is started, the objective is displayed and the player randomly placed on an edge of
+** the map, which is output to the screen. The player has the option to move to a different
+** location on the map via pointers to adjacent spaces in each space object on the map. Moving
+** to a new location marks the space as discovered, which allows the user to view the space
+** on the map. The player can also search the current space for a chance to reveal adjacent
+** spaces and search for items to add to their backpack. Items replenish energy, which is
+** decreased as they move to adjacent spaces, with differing energy costs for different
+** spaces. The player must search until finding the radio, while continuing to move around the
+** map to reveal spaces, one of which has a trapped hiker in an unknown random location on the
+** map. The player must conserve energy by selectively navigating to different spaces, searching
+** to replenish energy, and then use the radio while in the same space as the hiker to win the
+** game.
 ***********************************************************************************************/
 
 #include <cstdlib>
-#include <iostream>
 #include <memory>
-#include <string>
 
 #include "Menu.hpp"
 #include "Game.hpp"
 
 using std::cout;
 using std::endl;
-using std::string;
 using std::unique_ptr;
 
 void populateMenu(unique_ptr<Menu> &menu);
